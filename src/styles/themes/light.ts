84a735dcc1d99951ff4513667b6c58d992c28fda
Rodrigo Bighetti <robighetti@gmail.com>
2020-11-28 10:19:47 -0300

    desenvolvimento do padrao de temas

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#dcdcdc',
+    secondary: '#fff',
+    tertiary: '#f5f5f5',
+
+    white: '#000',
+    back: '#fff',
+    gray: '#bfbfbf',
+
+    success: '#03bb85',
+    info: '#3b5998',
+    warning: '#ff6961',
+  },
+};
